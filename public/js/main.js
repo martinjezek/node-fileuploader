8f4215b4dadb4df271fd355963e8746d3dc5381e
Martin Jezek <info@martin-jezek.com>
2014-09-25 20:42:54 +0100

    feat: add upload submit event locker + visual effects #5

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,15 @@
 
 var $uploadList = $('#upload-list');
 var $progressbar = $('#progress-bar');
+var $submit = $('#submit');
+var $name = $('#name');
+var $file = $('#file');
+
 var uploadItemTemplate = Handlebars.compile($('#uploadItemTemplate').html());
 var progressbarTemplate = Handlebars.compile($('#progressbarTemplate').html());
 
 var socket = io.connect();
+var uploading = false;
 
 socket.on('ready', function(sid) {
     $('#sid').val(sid);
@@ -13,33 +18,42 @@ socket.on('ready', function(sid) {
 
 socket.on('progress', function(percent) {
     $progressbar.html(progressbarTemplate(percent));
-    console.log(percent);
 });
 
 $('form').on('submit', function() {
-    $.ajax({
-        url: '/',
-        type: 'POST',
-        data: new FormData(this),
-        enctype: 'multipart/form-data',
-        processData: false,
-        contentType: false,
-        cache: false,
-        beforeSend: function() {
-            $progressbar.html(progressbarTemplate(0));
-            $progressbar.addClass('active');
-        },
-        success: function(file) {
-            if (file.path !== null) {
-                // render uploaded item
-                var $uploadItem = $(uploadItemTemplate(file));
-                $uploadList.prepend($uploadItem);
-                // reset form inputs
-                $('#name').val('');
-                $('#file').val('');
+    if (!uploading) {
+        uploading = !uploading;
+
+        $.ajax({
+            url: '/',
+            type: 'POST',
+            data: new FormData(this),
+            enctype: 'multipart/form-data',
+            processData: false,
+            contentType: false,
+            cache: false,
+            timeout: 2*60*60*1000, // 2 hours for big files
+            beforeSend: function() {
+                $progressbar.html(progressbarTemplate(0));
+                $progressbar.addClass('active');
+                $submit.addClass('disabled');
+            },
+            success: function(file) {
+                if (file.path !== null) {
+                    // render uploaded item
+                    var $uploadItem = $(uploadItemTemplate(file));
+                    $uploadList.prepend($uploadItem);
+                }
+            },
+            complete: function() {
+                // reset form
+                $name.val('');
+                $file.val('');
+                $submit.removeClass('disabled');
+                $progressbar.removeClass('active');
+                uploading = !uploading;
             }
-            $progressbar.removeClass('active');
-        }
-    });
+        });
+    }
     return false;
 });
